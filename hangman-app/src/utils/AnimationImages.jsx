5dea0f85447c2ba7793d27cb579e4a9631d25a2b
Woleen <marcin.dadon@gmail.com>
2024-03-19 11:43:13 +0100

    Addet util for Animation Images

---
+++ hangman-app/src/utils/AnimationImages.jsx
@@ -0,0 +1,21 @@
+import a0Img from '../assets/animation/a0.png';
+import a1Img from '../assets/animation/a1.png';
+import a2Img from '../assets/animation/a2.png';
+import a3Img from '../assets/animation/a3.png';
+import a4Img from '../assets/animation/a4.png';
+import a5Img from '../assets/animation/a5.png';
+import a6Img from '../assets/animation/a6.png';
+import a7Img from '../assets/animation/a7.png';
+
+const animationImages = [
+  a0Img,
+  a1Img,
+  a2Img,
+  a3Img,
+  a4Img,
+  a5Img,
+  a6Img,
+  a7Img,
+];
+
+export default animationImages;
