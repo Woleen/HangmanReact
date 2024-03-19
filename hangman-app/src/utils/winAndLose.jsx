165d5d794f51a3787078079a5063852a0450d5c4
Woleen <marcin.dadon@gmail.com>
2024-03-19 15:06:09 +0100

    Added util for win and lose

---
+++ hangman-app/src/utils/winAndLose.jsx
@@ -0,0 +1,10 @@
+import winImg from '../assets/gameover/win.png';
+import gameoverImg from '../assets/gameover/gameover.png';
+
+
+const winAndLoseImages = {
+    win: winImg,
+    gameover: gameoverImg
+};
+
+export default winAndLoseImages;
